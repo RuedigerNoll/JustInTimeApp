6846f45ad2151d9add392c0108e3bcdacefc5e53
ruediger-noll@web.de <ruediger-noll@web.de>
2020-11-17 09:38:23 +0100

    Remove Web projects, add SharedAssemblyInfo support, Update Projects to .net framework 4.8

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.0000.0")]
+[assembly: AssemblyFileVersion("2.0.0000.0")]
